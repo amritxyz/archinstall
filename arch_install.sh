4d3fc11cb194d11681e368024c4e5bc881af12ae
nyx-void <amrit44404@proton.me>
2024-09-29 15:24:17 +0545

    idontknow

--- arch_install.sh
+++ arch_install.sh
@@ -4,18 +4,31 @@
 printf '\033c'
 echo "Welcome to void's Arch installer script"
 
+# Function to handle errors
+handle_error() {
+    echo "$1" && exit 1
+}
+
+# Safety confirmation for drive selection
+echo "Available drives:"
+lsblk
+echo "Enter the drive (e.g., /dev/sda): "
+read -r drive
+
+echo "This will delete all existing partitions on $drive. Proceed? (y/n)"
+read -r confirmation
+if [[ "$confirmation" != "y" ]]; then
+    echo "Operation canceled."
+    exit 1
+fi
+
 # Update pacman config for parallel downloads
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 
 # Ensure the archlinux keyring is up to date
-pacman --noconfirm -Sy archlinux-keyring
+pacman --noconfirm -Sy archlinux-keyring || handle_error "Failed to update archlinux keyring"
 loadkeys us
 timedatectl set-ntp true
-
-# Show available drives
-lsblk
-echo "Enter the drive (e.g., /dev/sda): "
-read drive
 
 # Partition the drive
 echo "Creating partitions..."
@@ -32,33 +45,32 @@ echo 2      # Partition number
 echo        # Default - first sector
 echo        # Use the rest of the disk
 echo w      # Write changes
-) | fdisk $drive || { echo "Partitioning failed."; exit 1; }
+) | fdisk "$drive" || handle_error "Partitioning failed."
 
 # Format the partitions automatically
 echo "Formatting partitions..."
 efipartition="${drive}1"
 rootpartition="${drive}2"
 
-mkfs.vfat -F 32 $efipartition || { echo "Failed to format the EFI partition."; exit 1; }
-mkfs.ext4 $rootpartition || { echo "Failed to format the root partition."; exit 1; }
+mkfs.vfat -F 32 "$efipartition" || handle_error "Failed to format the EFI partition."
+mkfs.ext4 "$rootpartition" || handle_error "Failed to format the root partition."
 
 # Mount the root partition
-mount $rootpartition /mnt || { echo "Failed to mount the root partition."; exit 1; }
+mount "$rootpartition" /mnt || handle_error "Failed to mount the root partition."
 
 # Install base system
-pacstrap /mnt base base-devel linux linux-firmware || { echo "Base installation failed."; exit 1; }
+pacstrap /mnt base base-devel linux linux-firmware || handle_error "Base installation failed."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Prepare for chroot
-sed '1,/^#part2$/d' `basename $0` > /mnt/arch_install2.sh
+sed '1,/^#part2$/d' "$(basename "$0")" > /mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
 exit
 
 # part2
 printf '\033c'
-pacman -S --noconfirm sed
-sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+pacman -S --noconfirm sed || handle_error "Failed to install sed"
 
 # Set timezone and locale
 ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
@@ -71,79 +83,72 @@ echo "KEYMAP=us" > /etc/vconsole.conf
 
 # Hostname setup
 echo "Enter hostname: "
-read hostname
-echo $hostname > /etc/hostname
+read -r hostname
+echo "$hostname" > /etc/hostname
 {
   echo "127.0.0.1       localhost"
   echo "::1             localhost"
   echo "127.0.1.1       $hostname.localdomain $hostname"
 } >> /etc/hosts
 
-mkinitcpio -P
+mkinitcpio -P || handle_error "Failed to generate initramfs"
 passwd
 
 # Install GRUB
-pacman --noconfirm -S grub efibootmgr
+pacman --noconfirm -S grub efibootmgr || handle_error "Failed to install GRUB and efibootmgr"
 mkdir /boot/efi
-mount $efipartition /boot/efi || { echo "Failed to mount EFI partition."; exit 1; }
+mount "$efipartition" /boot/efi || handle_error "Failed to mount EFI partition."
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB || { echo "GRUB installation failed."; exit 1; }
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB || handle_error "GRUB installation failed."
 sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Install packages
-# efibootmgr base base-devel intel-ucode xorg-xbacklight xorg-xkill gst-plugin-pipewire libpulse wireplumber
-# mesa libva-intel-driver intel-media-driver intel-media-driver vulkan-intel
-# slock
+grub-mkconfig -o /boot/grub/grub.cfg || handle_error "Failed to generate GRUB configuration."
 
 # Install essential packages
 pacman -S --noconfirm \
-	xorg-server xorg-xinit xorg-xset xorg-xprop \
-	brightnessctl xwallpaper htop lf xdotool alsa-utils \
-	ttf-font-awesome ttf-hack ttf-hack-nerd noto-fonts-emoji xcompmgr fastfetch \
-	firefox nsxiv neovim mpv newsboat bleachbit unzip zathura zathura-pdf-poppler \
-	libxft libxinerama scrot xf86-video-intel bluez bluez-utils unclutter xclip \
-	zip unzip pipewire pipewire-alsa pipewire-pulse \
-	networkmanager bluez bluez-utils libxft libxinerama
+    xorg-server xorg-xinit xorg-xset xorg-xprop \
+    brightnessctl xwallpaper htop lf xdotool alsa-utils \
+    ttf-font-awesome ttf-hack ttf-hack-nerd noto-fonts-emoji xcompmgr fastfetch \
+    firefox nsxiv neovim mpv newsboat bleachbit unzip zathura zathura-pdf-poppler \
+    libxft libxinerama scrot xf86-video-intel bluez bluez-utils unclutter xclip \
+    zip unzip pipewire pipewire-alsa pipewire-pulse || handle_error "Failed to install essential packages."
 
 # Enable services
-systemctl enable NetworkManager.service
+systemctl enable NetworkManager.service || handle_error "Failed to enable NetworkManager service"
 
 # Configure sudo
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # User setup
 echo "Enter Username: "
-read username
-useradd -m -G wheel -s /bin/bash $username
-passwd $username
+read -r username
+useradd -m -G wheel -s /bin/bash "$username"
+passwd "$username"
 
 # Prepare for next part
 echo "Pre-Installation Finished. Reboot now."
-ai3_path=/home/$username/arch_install3.sh
-sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
-chown $username:$username $ai3_path
-chmod +x $ai3_path
-su -c $ai3_path -s /bin/sh $username
+ai3_path="/home/$username/arch_install3.sh"
+sed '1,/^#part3$/d' arch_install2.sh > "$ai3_path"
+chown "$username:$username" "$ai3_path"
+chmod +x "$ai3_path"
+su -c "$ai3_path" -s /bin/sh "$username"
 exit
 
 # part3
 printf '\033c'
-cd $HOME
+cd "$HOME" || exit
 
-git clone --depth=1 https://github.com/nyx-void/archrice $HOME/archrice
-mkdir -p $HOME/.local/share $HOME/.config $HOME/.local/src $HOME/.local/bin $HOME/.local/hugo-dir
+git clone --depth=1 https://github.com/nyx-void/archrice "$HOME/archrice" || handle_error "Failed to clone archrice"
+mkdir -p "$HOME/.local/share" "$HOME/.config" "$HOME/.local/src" "$HOME/.local/bin" "$HOME/.local/hugo-dir"
 
 # Copying Dotfiles
 echo "=> copying configs from dotfiles"
-
-cp -r $HOME/archrice/.local/share/* $HOME/.local/share
-cp $HOME/archrice/.local/bin/* $HOME/.local/bin
-cp -r $HOME/archrice/.config/* $HOME/.config
-cp $HOME/archrice/.bashrc $HOME/.bashrc
-cp $HOME/archrice/.inputrc $HOME/.inputrc
-cp $HOME/archrice/.xinitrc $HOME/.xinitrc
+cp -r "$HOME/archrice/.local/share/"* "$HOME/.local/share/" || handle_error "Failed to copy share files"
+cp "$HOME/archrice/.local/bin/"* "$HOME/.local/bin/" || handle_error "Failed to copy bin files"
+cp -r "$HOME/archrice/.config/"* "$HOME/.config/" || handle_error "Failed to copy config files"
+cp "$HOME/archrice/.bashrc" "$HOME/.bashrc" || handle_error "Failed to copy .bashrc"
+cp "$HOME/archrice/.inputrc" "$HOME/.inputrc" || handle_error "Failed to copy .inputrc"
+cp "$HOME/archrice/.xinitrc" "$HOME/.xinitrc" || handle_error "Failed to copy .xinitrc"
 
 # Install window manager and other applications
 declare -a repos=(
@@ -155,10 +160,13 @@ declare -a repos=(
 )
 
 for repo in "${repos[@]}"; do
-  git clone --depth=1 "$repo" ~/.local/src/$(basename "$repo" .git)
-  cd ~/.local/src/$(basename "$repo" .git) || exit
-  sudo make install
-  cd -
+  git clone --depth=1 "$repo" "$HOME/.local/src/$(basename "$repo" .git)" || handle_error "Failed to clone $(basename "$repo")"
+  cd "$HOME/.local/src/$(basename "$repo" .git)" || handle_error "Failed to enter directory for $(basename "$repo")"
+  sudo make install || handle_error "Failed to install $(basename "$repo")"
+  cd - || exit
 done
 
-exit
+# Cleanup: Remove temporary installation scripts
+rm -f /mnt/arch_install2.sh
+
+echo "Installation complete. Please reboot."
